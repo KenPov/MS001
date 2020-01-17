b46a2b125c8153a462936568de7b57d26e68f600
Ken Pov <50474003+KenPov@users.noreply.github.com>
2020-01-17 15:25:44 +0700

    Chapter 1: Hello, World! (v0.1)

---
+++ MS001.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Worlds!'
+
+if __name__ == '__main__':
+    app.run()
